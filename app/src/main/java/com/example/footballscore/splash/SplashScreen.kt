482566a82f640aeae5466f988aeea7886e667626
dmt231 <dominhtue123@gmail.com>
2023-12-25 16:35:50 +0700

    Update

--- app/src/main/java/com/example/footballscore/splash/SplashScreen.kt
+++ app/src/main/java/com/example/footballscore/splash/SplashScreen.kt
@@ -1,12 +1,22 @@
 package com.example.footballscore.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.footballscore.MainActivity
 import com.example.footballscore.R
 
 class SplashScreen : AppCompatActivity() {
+
+    private val delayTime : Long = 650
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayTime)
     }
 }
